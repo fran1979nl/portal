69fc7a7b56473fafcdbfdfe8a1d4d170d32fcc99
Francisco Rivolta <franrivolta@MacBook-Air-de-Francisco.local>
2025-09-16 14:53:27 +0200

    test(e2e): start Next server via Playwright webServer in CI

--- playwright.config.ts
+++ playwright.config.ts
@@ -5,6 +5,12 @@ export default defineConfig({
   reporter: 'list',
   use: {
     baseURL: 'http://localhost:3000',
-    trace: 'on-first-retry'
+    trace: 'on-first-retry',
+  },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+    timeout: 120_000
   },
 });
